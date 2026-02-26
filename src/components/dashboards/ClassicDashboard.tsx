f01233d4e62528f9e7f682ed7924d7f4d63d6d11
ibrahimcowke <ibrahimcowke@users.noreply.github.com>
2026-02-26 16:45:21 +0300

    feat: move Hadith slider below counter on mobile in Classic layout

--- src/components/dashboards/ClassicDashboard.tsx
+++ src/components/dashboards/ClassicDashboard.tsx
@@ -11,8 +11,12 @@ const ClassicDashboard = () => {
                 <Counter />
             </div>
             <div className="w-full max-w-2xl grid grid-cols-1 md:grid-cols-2 gap-6">
-                <StatsWidget />
-                <HadithSlider dhikr={currentDhikr} />
+                <div className="order-2 md:order-1">
+                    <StatsWidget />
+                </div>
+                <div className="order-1 md:order-2">
+                    <HadithSlider dhikr={currentDhikr} />
+                </div>
             </div>
         </div>
     );
